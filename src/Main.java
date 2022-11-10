bc58b36cc64a75481e94514f764a8f0563b50a10
Kamil <k.planeta20@gmail.com>
2022-11-10 21:59:57 +0100

    big update

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
+import java.io.FileWriter;
+
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
-        gsgsgdfjh
+        MyThread thread = new MyThread('a');
+        MyThread thread2 = new MyThread('b');
+        thread.start();
+        thread2.start();
+
+
+
+        /*
+    try {
+
+        FileWriter f = new FileWriter("fileThreads.txt");
+    }catch (Exception e) {
+        System.out.println("ERROR");
+    }
+    */
+
     }
 }
